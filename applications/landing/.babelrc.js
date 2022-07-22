63c82d1da2d7dce399868dfe91ca295d35ec4dcb
Patrice Juergens <pj@validitylabs.org>
2022-07-22 14:25:05 +0200

    fix: add missing babel dependency

--- applications/landing/.babelrc.js
+++ applications/landing/.babelrc.js
@@ -1,24 +1,40 @@
 const plugins = [
-    [
-      'babel-plugin-import',
-      {
-        libraryName: '@mui/material',
-        libraryDirectory: '',
-        camel2DashComponentName: false,
-      },
-      'core',
-    ],
-    [
-      'babel-plugin-import',
-      {
-        libraryName: '@mui/icons-material',
-        libraryDirectory: '',
-        camel2DashComponentName: false,
-      },
-      'icons',
-    ],
-  ];
+  [
+    'babel-plugin-import',
+    {
+      libraryName: '@mui/material',
+      libraryDirectory: '',
+      camel2DashComponentName: false
+    },
+    'core'
+  ],
+  [
+    'babel-plugin-import',
+    {
+      libraryName: '@mui/icons-material',
+      libraryDirectory: '',
+      camel2DashComponentName: false
+    },
+    'icons'
+  ],
+  [
+    'import',
+    {
+      libraryName: 'react-use',
+      camel2DashComponentName: false,
+      customName(/** @type {string} */ name) {
+        const libraryDirectory = name.startsWith('Use')
+          ? 'lib/component'
+          : name.startsWith('create')
+          ? 'lib/factory'
+          : 'lib';
+        return `react-use/${libraryDirectory}/${name}`;
+      }
+    },
+    'import-react-use'
+  ]
+];
 
-  const presets = ['next/babel'];
+const presets = ['next/babel'];
 
-  module.exports = { plugins, presets };
+module.exports = { plugins, presets };
