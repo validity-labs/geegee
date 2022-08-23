1ced8f7567415f22c424d9974979fed5299beb15
kaitoyamamoto <kaitoyamamoto931105@gmail.com>
2022-08-23 14:39:09 +0100

    feat: update geegee contract with existing account mode

--- sc/contract-geegee/src/lib.rs
+++ sc/contract-geegee/src/lib.rs
@@ -3,15 +3,17 @@ Some hypothetical DeFi contract that will do smart things with the transferred t
 */
 use near_contract_standards::fungible_token::receiver::FungibleTokenReceiver;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::UnorderedMap;
 use near_sdk::json_types::U128;
 use near_sdk::{
-    env, ext_contract, log, near_bindgen, AccountId, Gas, Promise, Balance, PanicOnDefault, PromiseOrValue
+    env, ext_contract, log, near_bindgen, AccountId, Balance, Gas, PanicOnDefault, Promise,
+    PromiseOrValue,
 };
-use near_sdk::collections::{UnorderedMap};
 
-pub const INIT_GAS: u64 = 10_000_000_000_000;
-pub const INITIAL_DEPOSIT_AMOUNT: U128 = U128(9_000_000_000_000_000_000_000);
-const MIN_STORAGE: Balance = 1_000_000_000_000_000_000_000;
+pub const INIT_GAS: Balance = 10_000_000_000_000;
+pub const INITIAL_ATTACHED_AMOUNT: Balance = 9_000_000_000_000_000_000_000;
+pub const STORAGE_COST: Balance = 1_000_000_000_000_000_000_000;
+pub const GEEBUCK_STARTING_AMOUNT: Balance = 50_000_000_000;
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
@@ -22,16 +24,8 @@ pub struct GeeGee {
 
 #[ext_contract(ext_tf)]
 pub trait GeeBuckToken {
-    fn ft_transfer(
-        &mut self,
-        receiver_id: AccountId,
-        amount: U128,
-    );
-    fn storage_deposit(
-        &mut self,
-        account_id: Option<AccountId>,
-        registeration_only: Option<bool>,
-    );
+    fn ft_transfer(&mut self, receiver_id: AccountId, amount: U128);
+    fn storage_deposit(&mut self, account_id: Option<AccountId>, registeration_only: Option<bool>);
 }
 
 #[near_bindgen]
@@ -41,9 +35,9 @@ impl GeeGee {
     pub fn new(geebuck_account_id: AccountId) -> Self {
         assert!(!env::state_exists(), "Already initialized");
         let promise = ext_tf::ext(geebuck_account_id.clone())
-            .with_attached_deposit(INITIAL_DEPOSIT_AMOUNT.into())
+            .with_attached_deposit(INITIAL_ATTACHED_AMOUNT.into())
             .storage_deposit(Some(env::current_account_id()), None);
-        
+
         Self {
             geebuck_account_id: geebuck_account_id.into(),
             registered_users: UnorderedMap::new(b"d"),
@@ -51,20 +45,65 @@ impl GeeGee {
     }
 
     #[private]
-    pub fn register_user(&self, user_id: String) {
+    #[payable]
+    pub fn register_user(&mut self, user_id: String) {
+        let total_required_cost =
+            STORAGE_COST * 2 + INITIAL_ATTACHED_AMOUNT + GEEBUCK_STARTING_AMOUNT;
+        let attached_amount: Balance = env::attached_deposit();
+
+        assert!(
+            attached_amount > total_required_cost,
+            "Attach at least {} yoctoNEAR",
+            total_required_cost
+        );
+
         let account_id = user_id + "." + &env::current_account_id().to_string();
-        
-        let promise = Promise::new(account_id.parse().unwrap()).create_account().transfer(MIN_STORAGE);
         let user_account: AccountId = account_id.parse().unwrap();
-        
+
+        let mut is_registered = self.registered_users.get(&user_account).unwrap_or(false);
+        assert!(!is_registered, "Already registered");
+
+        let promise = Promise::new(account_id.parse().unwrap())
+            .create_account()
+            .transfer(STORAGE_COST);
+
+        promise
+            .then(
+                ext_tf::ext(self.geebuck_account_id.clone())
+                    .with_attached_deposit(INITIAL_ATTACHED_AMOUNT.into())
+                    .storage_deposit(Some(user_account.clone()), None),
+            )
+            .then(
+                ext_tf::ext(self.geebuck_account_id.clone())
+                    .with_attached_deposit(1)
+                    .ft_transfer(user_account, U128(GEEBUCK_STARTING_AMOUNT)),
+            );
+    }
+
+    #[private]
+    #[payable]
+    pub fn register_account(&mut self, user_account: AccountId) {
+        let total_required_cost = STORAGE_COST + INITIAL_ATTACHED_AMOUNT + GEEBUCK_STARTING_AMOUNT;
+        let attached_amount: Balance = env::attached_deposit();
+
+        assert!(
+            attached_amount > total_required_cost,
+            "Attach at least {} yoctoNEAR",
+            total_required_cost
+        );
+
+        let mut is_registered = self.registered_users.get(&user_account).unwrap_or(false);
+        assert!(!is_registered, "Already registered");
+
+        let promise = ext_tf::ext(self.geebuck_account_id.clone())
+            .with_attached_deposit(INITIAL_ATTACHED_AMOUNT.into())
+            .storage_deposit(Some(user_account.clone()), None);
         promise.then(
             ext_tf::ext(self.geebuck_account_id.clone())
-            .with_attached_deposit(INITIAL_DEPOSIT_AMOUNT.into())
-            .storage_deposit(Some(user_account.clone()), None)
-        ).then(
-            ext_tf::ext(self.geebuck_account_id.clone())
-            .with_attached_deposit(1)
-            .ft_transfer(user_account, U128(50_000_000_000))
+                .with_attached_deposit(1)
+                .ft_transfer(user_account, U128(GEEBUCK_STARTING_AMOUNT)),
         );
     }
+
+    pub fn geebuck_transfer(&self, receiver_id: String, amount: Balance) {}
 }
